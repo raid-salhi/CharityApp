f83102d9f1e42e2ae280068e63254a94e5c1a89b
raid-salhi <118809948+raid-salhi@users.noreply.github.com>
2023-03-02 17:47:26 +0100

    Revert "I added the drawer layout"
    This reverts commit 9f34e4817dde8c80f8ed26c482e7646a70101220.

--- app/src/main/java/com/example/charityapp/MainActivity.kt
+++ app/src/main/java/com/example/charityapp/MainActivity.kt
@@ -3,16 +3,15 @@ package com.example.charityapp
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
-import androidx.navigation.ui.*
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.charityapp.databinding.ActivityMainBinding
-import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,24 +19,17 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val bottomNavView: BottomNavigationView = binding.bottomNavView
-        val drawerLayout: DrawerLayout = binding.drawerLayout
-        val navView: NavigationView = binding.navView
+        val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(
-            setOf(R.id.navigation_home,R.id.navigation_emergency,R.id.navigation_event),
-            drawerLayout
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_emergency,R.id.navigation_event,R.id.navigation_need_help
+            )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
-
-        bottomNavView.setupWithNavController(navController)
         navView.setupWithNavController(navController)
-
-    }
-    override fun onSupportNavigateUp(): Boolean {
-        return findNavController(R.id.nav_host_fragment_activity_main).navigateUp(appBarConfiguration)
     }
 }
