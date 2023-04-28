a394737e752b0ca3923590acd5d6919d6eae5476
Hachemi Ibrahim <123593839+HachemiIbrahim@users.noreply.github.com>
2023-04-28 01:53:58 +0100

    needHelpUI

---
+++ app/src/main/java/com/example/charityapp/ui/needhelp/NeedHelpFragment.kt
@@ -0,0 +1,30 @@
+package com.example.charityapp.ui.needhelp
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.charityapp.R
+
+class NeedHelpFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = NeedHelpFragment()
+    }
+
+    private lateinit var viewModel: NeedHelpViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_need_help, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(NeedHelpViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/charityapp/ui/needhelp/NeedHelpViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.charityapp.ui.needhelp
+
+import androidx.lifecycle.ViewModel
+
+class NeedHelpViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
