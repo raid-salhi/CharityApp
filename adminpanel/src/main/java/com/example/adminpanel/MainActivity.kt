a66e4451c77ffae09cd9f49e8f605476fba2b670
raid-salhi <118809948+raid-salhi@users.noreply.github.com>
2023-05-04 22:15:56 +0100

    changed main activity

--- adminpanel/src/main/java/com/example/adminpanel/MainActivity.kt
+++ adminpanel/src/main/java/com/example/adminpanel/MainActivity.kt
@@ -2,15 +2,26 @@ package com.example.adminpanel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.example.adminpanel.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val bottomNavView: BottomNavigationView = binding.bottomNavView
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        bottomNavView.setupWithNavController(navController)
 
     }
 
